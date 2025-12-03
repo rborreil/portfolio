85c231d765807df26b99b0aad440c36aad1f7846
Robin Borreil <robinborreil.dev@gmail.com>
2025-12-03 13:36:05 +0100

    Logo + Section Services + Section Tarifs + Boutons

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 // SCROLL TO TOP
 // Obtenir le bouton
 const scrollToTopBtn = document.getElementById("scroll_to_top");
-
+const fixedBtn = document.querySelector(".fixed-btn");
 
 // Définir le nombre de pixels à partir duquel le bouton doit apparaître
 const scrollThreshold = 650; // Ajustez cette valeur selon vos préférences
 
-
 // Fonction pour afficher/masquer le bouton
-function toggleScrollToTopBtn() {
+function toggleFixedButtons() {
   if (window.pageYOffset > scrollThreshold) {
     scrollToTopBtn.style.display = "block";
+    fixedBtn.style.display = "block";
   } else {
     scrollToTopBtn.style.display = "none";
+    fixedBtn.style.display = "none";
   }
 }
 
 // Ajouter un écouteur d'événement pour détecter le scroll
-window.addEventListener("scroll", toggleScrollToTopBtn);
+window.addEventListener("scroll", toggleFixedButtons);
 
 // Fonction pour faire défiler vers le haut de la page
 function scrollToTop() {
@@ -27,10 +28,21 @@ function scrollToTop() {
 // Ajouter un écouteur d'événement pour le clic sur le bouton
 scrollToTopBtn.addEventListener("click", scrollToTop);
 
-
 // DARK MODE
-const toggleDarkMode = document.querySelector('.dark-light');
+// const toggleDarkMode = document.querySelector('.dark-light');
 
-toggleDarkMode.addEventListener('change', () => {
-  document.body.classList.toggle('dark-mode', toggleDarkMode.checked);
-})
+// toggleDarkMode.addEventListener('change', () => {
+//   document.body.classList.toggle('dark-mode', toggleDarkMode.checked);
+// })
+
+
+
+// Empêcher Clic Droit
+// document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+// Empêcher CTRL+C, CTRL+U, F12
+// document.addEventListener("keydown", function (e) {
+//   if (e.ctrlKey && ["u", "U", "c", "C", "s", "S"].includes(e.key)) {
+//     e.preventDefault();
+//   }
+// });
